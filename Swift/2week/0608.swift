7793c9b7ae1971f8937bee9f6d00b6f4ec54cdba
susu <skyqnaqna@naver.com>
2021-06-09 03:43:23 +0900

    Update 0608.swift

--- Swift/2week/0608.swift
+++ Swift/2week/0608.swift
@@ -17,3 +17,27 @@ class Person {
 
 var person1: Person? = Person()
 person1 = nil // Person 클래스의 인스턴스가 소멸됩니다.
+
+// 값 타입과 참조 타입의 차이
+struct BasicInfo {
+  let name: String
+  var age: Int
+}
+
+// 값 복사 할당
+var aaInfo = BasicInfo(name: "aa", age: 38)
+var bbInfo = aaInfo
+bbInfo.age = 20
+
+print(aaInfo.age, bbInfo.age) // 38 20
+
+// 참조 할당
+var aaPerson = Person()
+var bbPerson = aaPerson
+
+bbPerson.height = 170.7
+
+print(aaPerson.height, bbPerson.height) // 170.7 170.7
+
+// 식별 연산자 (Identity Operations)
+print(aaPerson === bbPerson) // true

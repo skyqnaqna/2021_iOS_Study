b9d25af18e7cbacdf4c938019ae6b5fd64f1d1c7
susu <skyqnaqna@naver.com>
2021-08-01 16:17:14 +0900

    Update cardMatching function

--- Stanford/Concentration/Concentration/ViewController.swift
+++ Stanford/Concentration/Concentration/ViewController.swift
@@ -46,11 +46,15 @@ class ViewController: UIViewController {
     }
   }
   
-  var emojiChoices = ["👻", "🎃", "🎃", "👻"]
+  var emojiChoices = ["👻", "🎃", "😈", "😺", "👺", "🍎", "🍭", "🦇", "🐶"]
   
   var emoji = [Int:String]()
 
   func emoji(for card: Card) -> String {
+    if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+      let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+      emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+    }
     return emoji[card.identifier] ?? "?"
   }
 
